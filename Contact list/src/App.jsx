335895f2982c760d092720a23d29d3275a3f8cbe
Behzad Dominic <58052010+Superhero23ufo@users.noreply.github.com>
2024-02-29 17:56:39 -0800

    Update App.jsx

--- Contact list/src/App.jsx
+++ Contact list/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ContactDetails from './components/ContactDetails'
+import ContactList from './components/ContactList'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [selectedContactId, setSelectedContactId] = useState(null)
+	const [detailedContact, setDetailedContact] = useState(null)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		async function fetchSelectedContact() {
+			try {
+				const response = await fetch(
+					'https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/' +
+						selectedContactId
+				)
+				const result = await response.json()
+				setDetailedContact(result)
+			} catch (error) {
+				console.error(error)
+			}
+		}
+		if (selectedContactId) {
+			fetchSelectedContact()
+		}
+	}, [selectedContactId])
+
+	return (
+		<>
+			{detailedContact ? (
+				<ContactDetails detailedContact={detailedContact} />
+			) : (
+				<ContactList setSelectedContactId={setSelectedContactId} />
+			)}
+		</>
+	)
 }
 
 export default App
